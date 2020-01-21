c7066c46cec742516aec52939ca800a68c0146f6
Changdi <czhen08@163.com>
2020-01-21 11:29:38 +0000

    sadsad

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,13 +3,14 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+    	int a1, a2;
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("what is 14 plus 13")) {
-            return "27";
+        if (query.toLowerCase().contains("what is %d plus %d", a1, a2)) {
+            return "a1 + a2";
         }
         return "";
     }
